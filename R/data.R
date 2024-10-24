0a36913c921e2f8ae739366e94c508fe26c9b862
Rayyan Aamir <raam0001@student.monash.edu>
2024-10-24 23:11:06 +1100

    added documentation for the dataset

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Electric Vehicle Data
+#'
+#' @format ## 'clean_vehicle'
+#' A data frame with 181038 rows and 13 columns
+#' \describe{
+#'   \item{**county**}{Name of county from where the data is gathered}
+#'   \item{**city**}{Name of city from where the data is gathered}
+#'   \item{**state**}{Name of state from where the data is gathered}
+#'   \item{**postal_code**}{The postal code from where the data is present}
+#'   \item{**model_year**}{Manufacturing year of the model mentioned in the dataset}
+#'   \item{**make**}{Manufacturer of the vehicle}
+#'   \item{**model**}{Model Name of the mentioned vehicle}
+#'   \item{**electric_vehicle_type**}{Type of the electric vehicle present in the dataset}
+#'   \item{**clean_alternative_fuel_vehicle_cafv_eligibility**}{Clean Alternative Fuel Vehicle (CAFV) eligibility for the data present in this dataset}
+#'   \item{**electric_range**}{The electric range the car provides}
+#'   \item{**vehicle_location**}{Vehicle coordinates}
+#'   \item{**lon**}{Longitude of the vehicle's location}
+#'   \item{**lat**}{Latitude of the vehicle's location}
+#' }
+#' @source <https://www.kaggle.com/datasets/jainaru/electric-vehicle-population/data>
+"clean_vehicle"
