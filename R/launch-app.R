c3c78660408208604fbd4eaaaed70da722e5b9a0
Rayyan Aamir <raam0001@student.monash.edu>
2024-10-25 17:22:59 +1100

    added roxygen2 commenting and documentation for the function

--- R/launch-app.R
+++ R/launch-app.R
@@ -1,3 +1,9 @@
+#' Loading the Rshiny app
+#'
+#' @description
+#' This function launches the RShiny app which allows the user to explore the Electric Vehicle
+#' data for themselves and come upon their own conclusions.
+#'
 #' @export
 load_app <- function() {
   app_dir <- system.file("ev-app", package = "ElectricVehiclesR")
