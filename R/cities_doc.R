1893853d49acd85ad92f8eda0b1106e1f8f95d81
Rayyan Aamir <raam0001@student.monash.edu>
2024-10-26 14:34:47 +1100

    created documentation for the 2 new datasets and renamed old main documentation file name

---
+++ R/cities_doc.R
@@ -0,0 +1,10 @@
+#' City Names Data
+#'
+#' @format ## 'cities'
+#' A data frame with 4 rows and 3 columns
+#' \describe{
+#'   \item{**city**}{Name of city from where the data is gathered}
+#'   \item{**lat**}{The latitude value of the city}
+#'   \item{**long**}{The longitude value of the city}
+#' }
+"cities"
---
+++ R/grid_counts_simplified_doc.R
@@ -0,0 +1,9 @@
+#' Simplified Grid Counts Dataset For Leaflet Plot Creation
+#'
+#' @format ## 'grid_counts_simplified'
+#' A data frame with 542 rows and 2 columns
+#' \describe{
+#'   \item{**geometry**}{The grouped lat and long values representing a location}
+#'   \item{**count**}{The count of EVs in the respective grouped location}
+#' }
+"grid_counts_simplified"
